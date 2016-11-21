7fcd4a27399f413f11fbfbfaaf5930a7608acef3
admac21 <admac21@gmail.com>
2016-11-21 15:55:51 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 /**
  * Created by Andrew on 21/11/2016.
  */
+
 var http = require('http')
+
 var port = process.env.PORT || 1337;
 
-http.createServer(function(request, response){
-    response.writeHead(200, { 'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+http.createServer(function(request, response) {
+
+response.writeHead(200, { 'Content-Type': 'text/plain'});
+
+response.end('Hello World\n');
+
 }).listen(port);
 
     
